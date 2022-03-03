acb91cb02f84990a951d24c0981d335f6f0322e1
Nanda <nana.script.js@gmail.com>
2022-03-03 12:14:04 -0300

    Add Tag Entity, Repository, Service and Controller

---
+++ src/services/CreateTagService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from "typeorm"
+import { TagRepository } from "../repositories/TagRepository"
+
+interface ITagRequest {
+    name: string;
+}
+
+class CreateTagService {
+    async execute({ name }: ITagRequest) {
+        const tagRepositoy = getCustomRepository(TagRepository);
+        
+        if (!name) {
+            throw new Error("Incorrect name!");
+        }
+
+        const tagAlreadyExists = await tagRepositoy.findOne({
+            name
+        });
+
+        if (tagAlreadyExists) {
+            throw new Error("Tag already exists!");
+        }
+
+        const tag = tagRepositoy.create({ name });
+
+        await tagRepositoy.save(tag);
+
+        return tag;
+    }
+}
+
+export { CreateTagService }
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -9,27 +9,25 @@ interface IUserRequest {
 
 class CreateUserService {
     async execute({ name, email, admin }: IUserRequest) {
-        try {
-            const userRepository = getCustomRepository(UserRepository);
+        const userRepository = getCustomRepository(UserRepository);
     
-            if (!email) {
-                throw new Error("Invalid email!");
-            }
-            const userAlreadyExists = await userRepository.findOne({
-                email
-            });
-    
-            if (userAlreadyExists) {
-                throw new Error("User already exists!");
-            }
-    
-            const user = userRepository.create({ name, email, admin });
-            await userRepository.save(user);
-    
-            return user;
-        } catch (error) {
-            console.log(error.message);
+        if (!email) {
+            throw new Error("Invalid email!");
         }
+        
+        const userAlreadyExists = await userRepository.findOne({
+            email
+        });
+    
+        if (userAlreadyExists) {
+            throw new Error("User already exists!");
+        }
+    
+        const user = userRepository.create({ name, email, admin });
+        await userRepository.save(user);
+    
+        return user;
+
     }
 }
 
