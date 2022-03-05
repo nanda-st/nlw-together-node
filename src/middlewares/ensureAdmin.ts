2c8112189233e70844f63981681338504a2f404f
Nanda <nana.script.js@gmail.com>
2022-03-05 13:46:07 -0300

    Add ensureAuthenticated middleware

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepository } from "../repositories/UserRepository";
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
-    const admin = true;
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+    const { user_id } = req;
+    console.log(req.headers);
+
+    const userRepository = getCustomRepository(UserRepository);
+
+    const { admin } = await userRepository.findOne(user_id);
+    console.log(admin)
 
     if (admin) {
         return next();
